3d2845f85cba481abcd487b333bc1862050f0a1d
Hossein Amir <Hossein.amir@gmx.at>
2024-02-06 22:45:33 +0100

    docs: adding javadoc for repositories

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
@@ -7,5 +7,16 @@ import org.springframework.data.repository.CrudRepository;
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
 public interface NoteRepository extends CrudRepository<Note, String> {
+
+    /**
+     * Finds notes whose content contains the specified string.
+     * Purpose: To retrieve a list of notes that contain a given string in their content.
+     * Input: 'content' (String) - The text to search for within the notes' content.
+     * Output: List< Note> - A list of notes where the content contains the specified string.
+     * Errors: might throw exceptions related to invalid query syntax.
+     *
+     * @param content The string to search for within the notes' content.
+     * @return A list of notes that contain the specified string in their content.
+     */
     List<Note> findByContentContaining(String content);
 }
